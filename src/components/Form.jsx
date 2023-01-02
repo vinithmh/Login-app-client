ba12008598e7b59c40be9dc32fd2dad0c6fc488f
Vinith M H <vinithmh2000@gmail.com>
2023-01-03 02:13:32 +0530

    initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Input from "./Input";
+
+function Form() {
+  return (
+    <div>
+      <Input className="signupform" />
+    </div>
+  );
+}
+
+export default Form;
